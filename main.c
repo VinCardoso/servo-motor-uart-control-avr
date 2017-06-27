64f8a24de6c93e55a26e12f8cff75dab7d08aef4
Vinicius Cardoso <viniciuscardoso@Viniciuss-MacBook-Air.local>
2017-06-27 19:08:05 -0300

    Initial projetct

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * prova_git.c
+ *
+ * Created: 6/27/17 7:06:52 PM
+ * Author : viniciuscardoso
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
