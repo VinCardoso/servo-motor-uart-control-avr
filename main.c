ab54238147c81cd93ea4131f1524eaafe7e48a25
Vinicius Cardoso <viniciuscardoso@Viniciuss-MacBook-Air.local>
2017-06-27 19:27:34 -0300

    Print on scren the commands in a line

--- main.c
+++ main.c
@@ -4,11 +4,33 @@
 
 FILE lcd_str = FDEV_SETUP_STREAM(USART_putchar, NULL, _FDEV_SETUP_WRITE);
 
+unsigned char dado_recebido;
+
 int main(void)
 {
-    /* Replace with your application code */
+    
+	USART_Inic(MYUBRR);
+	stdout=&lcd_str;
+	
+	printf("Digite [t] [s] ou [e] \r");
+	
+	
     while (1) 
     {
+		dado_recebido=USART_Recebe();
+		
+		if(dado_recebido=='t')
+		{
+			printf("Temperatura \r");
+		}
+		else if(dado_recebido=='e')
+		{
+		printf("Minha Prova");
+		}
+		else
+		{
+			printf("%c", dado_recebido);
+		}
     }
 }
 
